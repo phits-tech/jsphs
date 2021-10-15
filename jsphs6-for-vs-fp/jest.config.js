fa619f75cc001e170f4b4c6b0ec2edd05e79db80
Siwawes Wongcharoen <siwaoh@gmail.com>
2021-10-15 16:14:13 +0700

    build: update jest config

--- jsphs6-for-vs-fp/jest.config.js
+++ jsphs6-for-vs-fp/jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/']
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/'
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/(?!d3)/'
+  ]
 }
