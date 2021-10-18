c852480f05e337ddebdd9790dbe33ec54bcc6595
Siwawes Wongcharoen <siwaoh@gmail.com>
2021-10-18 09:45:09 +0700

    feature: add more option for max - add condition to check the array member - point max holder to the first array member instead of Number.MIN_VALUE

--- jsphs6-for-vs-fp/src/1-max/s1-for.ts
+++ jsphs6-for-vs-fp/src/1-max/s1-for.ts
@@ -11,3 +11,16 @@ export const maxFor: MaxFunction = (numbers: number[]): number | undefined => {
 
   return maxSoFar
 }
+
+export const maxForOption2: MaxFunction = (numbers: number[]): number | undefined => {
+  if (numbers.length === 0) return undefined
+  if (numbers.length === 1) return numbers[0]
+
+  let maxSoFar = numbers[0]
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > maxSoFar) maxSoFar = numbers[i]
+  }
+
+  return maxSoFar
+}
